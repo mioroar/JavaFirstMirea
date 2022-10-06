2ed3347c68266798505b23af038af4f8d0e9a9ec
Maxim Matveenko <marikdorito@gmail.com>
2022-10-06 21:38:51 +0300

    fix

--- src/ru/mirea/task5/FirstGui.java
+++ src/ru/mirea/task5/FirstGui.java
@@ -5,35 +5,8 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class FirstGui {
-    public static void main(String[] args){
-        JFrame frame = new JFrame("My First Gui") ;//создаем окно с названием
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500,200);//размер окна
-        JButton button1 = new JButton("AC Milan");// создаем кнопку с надписью на ней
-        JButton button2 = new JButton("Real Madrid");// создаем кнопку с надписью на ней
-        //frame.getContentPane().add(button);//добавляем кнопку на окно
-        JPanel panel = new JPanel();
-        panel.setBackground(Color.GRAY);
-        panel.setPreferredSize(new Dimension(500,400));
-        panel.add(button1);
-        panel.add(button2);
-        JLabel label1 = new JLabel("Result: 0 X 0 ",JLabel.LEFT);
-        JLabel label2 = new JLabel("Last Scorer: N/A ",JLabel.CENTER);
-        JLabel label3 = new JLabel("Winner DRAW ",JLabel.RIGHT);
-        frame.setPreferredSize(new Dimension(500,600));
-        panel.add(label1,JLabel.VERTICAL);
-        panel.add(label2,JLabel.VERTICAL);
-        panel.add(label3,JLabel.VERTICAL);
-
-        frame.getContentPane().add(panel);
-        frame.pack();
-        int i=0;
-        //button1.addActionListener(() -> {
-         //   i++;
-        //});
-        frame.setVisible(true);
-
-
+    public static void main(String[] args) {
+        new Gui();
 
     }
 }
--- src/ru/mirea/task5/Gui.java
+++ src/ru/mirea/task5/Gui.java
@@ -1,11 +1,93 @@
 package ru.mirea.task5;
 
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.*;
 
-public class Gui extends JFrame {
-    private JButton button1 = new JButton("AC Milan");// создаем кнопку с надписью на ней
-    private JButton button2 = new JButton("Real Madrid");// создаем кнопку с надписью на ней
 
+public class Gui implements ActionListener {
+    private JFrame frame;
+    private JPanel panel;
+    private JPanel panel1;
+
+    private JLabel label1;
+    private JLabel label2;
+    private JLabel label3;
+
+    private JButton button1;
+    private JButton button2;
+
+    private int counter1;
+    private int counter2;
+
+
+    private enum Actions {
+        Milan,
+        Madrid
+    }
+
+
+    public Gui(){
+
+        frame = new JFrame();
+
+        label1 = new JLabel("Result 0 X 0");
+        label2 = new JLabel("Last scorer:");
+        label3 = new JLabel("Winner: DRAW");
+
+        button1 = new JButton("AC Milan");
+        button2 = new JButton("Real Madrid");
+        button1.addActionListener(this);
+        button1.setActionCommand(Actions.Milan.name());
+        button2.addActionListener(this);
+        button2.setActionCommand(Actions.Madrid.name());
+
+
+
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(50, 20, 10, 20));
+        panel.setLayout(new GridLayout(1, 0));
+        panel.add(button1);
+        panel.add(button2);
+
+        panel1 = new JPanel();
+        panel1.setBorder(BorderFactory.createEmptyBorder(50,50,10,10));
+        panel1.setLayout(new GridLayout(3,0));
+        panel1.add(label1);
+        panel1.add(label2);
+        panel1.add(label3);
+
+        frame.add(panel,BorderLayout.CENTER);
+        frame.add(panel1, BorderLayout.NORTH);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        if (event.getActionCommand() == "Milan") {
+            counter1++;
+            label1.setText("Result " + counter1 + " X " + counter2);
+            label2.setText("Last scorer: AC Milan");
+            if (counter1 > counter2)
+                label3.setText("Winner: AC Milan");
+            else if (counter1 < counter2)
+                label3.setText("Winner: Madrid");
+            else
+                label3.setText("Winner: Draw");
+        }
+        else if (event.getActionCommand() == "Madrid"){
+            counter2++;
+            label1.setText("Result " + counter1 + " X " + counter2);
+            label2.setText("Last scorer: Madrid");
+            if (counter1 > counter2)
+                label3.setText("Winner: Madrid");
+            else if (counter1 < counter2)
+                label3.setText("Winner: Madrid");
+            else
+                label3.setText("Winner: Draw");
+        }
+    }
 }
+
+
